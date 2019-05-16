f34a4d786ae5535cdae1e28e20c65624d4c7d4ac
Tom Bell <tjbell@gmail.com>
2019-05-16 15:33:34 -0600

    test: final tests

--- tests/tests_module1.R
+++ tests/tests_module1.R
@@ -1,5 +1,6 @@
 library(rlang)
 library(glue)
+library(stringi)
 
 setwd('..')
 
@@ -10,6 +11,8 @@ user <- new.env()
 source('download.R', local = user)
 
 parsed <- parse_exprs(file('download.R'))
+
+unique_books_called <- FALSE
 
 for (line in parsed) {
   arg_list <- call_args(line)
@@ -34,8 +37,9 @@ for (line in parsed) {
       }
     }
   }
-  
+
   plot_arg <- ifelse(is_call(line, 'plot', 1), arg_list, '')
+  
 }
 
 walkAST <- function(expr, args) {
@@ -44,20 +48,34 @@ walkAST <- function(expr, args) {
   } else {
     lines <- expr
   }
-  
+
   for (cc in seq_along(lines)) {
     line <- lines[[cc]]
     if(is_call(line)) {
       this_operator <- line[[1]]
       this_left <- line[[2]]
       this_right <- line[[3]]
-      if(this_left == args[[1]] && this_operator == args[[2]] &&  this_right == args[[3]]) {
+      
+      if(this_operator == 'if' && args[[1]] == 'if' && this_left == args[[2]] && typeof(this_right) == "language") {
+        if(is_list(args[[3]])) {
+          return(walkAST(this_right, args[[3]]))
+        } else {
+          return(TRUE)
+        }
+      }
+
+      str_this_operator <- stri_replace_all_fixed(deparse(line[[1]]), " ", "")
+      str_this_left <- stri_replace_all_fixed(deparse(line[[2]]), " ", "")
+      str_this_right <- stri_replace_all_fixed(deparse(line[[3]]), " ", "")
+      if(str_this_left == args[[1]] && str_this_operator == args[[2]] && str_this_right == args[[3]]) {
         return(TRUE)
       }
-      if(line[[1]] == 'for' && args[[1]] == 'for') {
-        print(line)
-        if(walkAST(line, args[[2]])) {
-          return(TRUE)
+      
+      if(this_operator == 'for' && args[[1]] == 'for') {
+        for (cc in seq_along(line)) {
+          if(typeof(line[[cc]]) == "language") {
+            return(walkAST(line[[cc]], args[[2]]))
+          }
         }
       }
     }
@@ -91,7 +109,7 @@ test_that('Create a Function @create-function', {
 
 test_that('Pull the Correct Column @pull', {
   expect(is_function(user$unique_books), 'Have you defined a function called `unique_books`?')
-  alternative <- walkAST(user$unique_books, list('items', '<-', 'data %>% pull(column)')) || walkAST(user$unique_books, list('items', '<-', 'pull(data, column)'))
+  alternative <- walkAST(user$unique_books, list('items', '<-', 'data%>%pull(column)')) || walkAST(user$unique_books, list('items', '<-', 'pull(data,column)'))
   expect(alternative, 'Have you `pull`ed the `column` and stored it in a varaible called `items`?')
 })
 
@@ -107,12 +125,14 @@ test_that(' Create a For Loop @for-loop', {
 
 test_that('Fuzzy Matching @fuzzy-matching', {
   expect(is_function(user$unique_books), 'Have you defined a function called `unique_books`?')
-  expect(walkAST(user$unique_books, list('for', list('match', '<-', 'agrep(item, items)'))), 'Have you created a `for` loop to loop through `items`?')
+  expect(walkAST(user$unique_books, list('for', list('match', '<-', 'agrep(item,items)'))), 'Are you using the `agrep` function to fuzzy match `item` in `items`?')
+  expect(walkAST(user$unique_books, list('for', list('last', '<-', 'match[-1]'))), 'Are you taking the last item from the `match` list?')
 })
 
 test_that('Add Elements to a List @add-list', {
   expect(is_function(user$unique_books), 'Have you defined a function called `unique_books`?')
-  
+  expect(walkAST(user$unique_books, list('for', list('if', 'length(last)', ''))), 'Do you have an `if` statement that checks the `length`` of the `match` list?')
+  expect(walkAST(user$unique_books, list('for', list('if', 'length(last)', list('duplicates[[last]]', '<-', 'last')))), 'In the body of an `if` statement are you adding the `last` variable to the `duplicates` list?')
 })
 
 test_that('Remove Duplicates @remove-duplicates', {
@@ -122,7 +142,9 @@ test_that('Remove Duplicates @remove-duplicates', {
 })
 
 test_that('Call a Function @call-function', {
-  
+  expect(is_function(user$unique_books), 'Have you defined a function called `unique_books`?')
+  expect('twain_unique' %in% ls(envir = user), 'Does the `twain_unique` data frame exist in `download.R`?')
+  expect(isTRUE(all_equal(user$twain_by_download, solution$twain_by_download)), 'The `twain_unique` data frame does not contain the correct data or columns. Have you called the `unique_books` function with the correct argument?')
 })
 
 test_that('Initialize a Plot Object @initialize-plot', {
