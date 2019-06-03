2b0590ada06bf8a6dba965c21d5fa03bbba72b1f
snychka <stefannychka@gmail.com>
2019-06-03 10:43:56 -0600

    finish off module

--- download.R
+++ download.R
@@ -3,6 +3,28 @@ library(dplyr)
 library(stringr)
 library(ggplot2)
 
+# https://swcarpentry.github.io/r-novice-inflammation/02-func-R/
+unique_books <- function(data, column='title') {
+  # a bit tough for me.  hints in test and task name (2.5)
+  items <- pull(data, column)
+  duplicates <- list()
+  # https://stackoverflow.com/questions/26297024/how-to-loop-through-a-list-in-r
+  for (item in items) {
+    match <- agrep(item, items)
+    last <- match[-1]
+    if (length(last)) {
+      duplicates[[last]] <- last
+    }
+  }
+  
+  # https://stackoverflow.com/questions/17094774/better-way-to-convert-list-to-vector
+  # remove <- unique(unlist(duplicates, use.names = FALSE))
+  remove <- unique(unlist(duplicates))
+  # guessed data - remove , which was wrong
+  # https://stackoverflow.com/questions/4835342/how-can-i-get-a-dataframe-with-columns-temporarily-removed-by-name
+  data[-remove,]
+}
+
 books <- read_csv('data/books.csv')
 
 # https://www.rdocumentation.org/packages/dplyr/versions/0.7.8/topics/filter
@@ -13,17 +35,14 @@ twain <- filter(books, author == 'Twain, Mark')
 # https://www.rdocumentation.org/packages/dplyr/versions/0.7.8
 twain_refined <- twain %>% select(title, author, downloads, avg_words_per_sentence, sentences)
 # https://dplyr.tidyverse.org/reference/arrange.html
-twain_by_download <-  arrange(twain_refined, desc(downloads))
+twain_by_download <- arrange(twain_refined, desc(downloads))
+
+twain_unique <- unique_books(twain_by_download)
+
+p <- ggplot(twain_unique, aes(sentences, avg_words_per_sentence)) + geom_point(aes(size=downloads))
+plot(p)
 
 
-# https://swcarpentry.github.io/r-novice-inflammation/02-func-R/
-unique_books <- function(data, column='title') {
-  # a bit tough for me.  hints in test and task name (2.5)
-  items <- pull(data, column)
-  duplicates <- list()
-  # https://stackoverflow.com/questions/26297024/how-to-loop-through-a-list-in-r
-  for (item in items) {
-    match <- agrep(item, items)
-    last <- match[-1]
-  }
-}
+
+
+
